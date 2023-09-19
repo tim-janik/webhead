8d34160430d6749914a04fd9c057a25b9400a3c7
Tim Janik <timj@gnu.org>
2023-09-19 03:18:26 +0200

    SRC: webhead.hh: add WebHeadSession
    Signed-off-by: Tim Janik <timj@gnu.org>

--- src/webhead.hh
+++ src/webhead.hh
@@ -1,6 +1,7 @@
 // This Source Code Form is licensed MPL-2.0: http://mozilla.org/MPL/2.0
 #pragma once
 
+#include <memory>
 #include <string>
 #include <vector>
 
@@ -23,5 +24,19 @@ struct WebHeadBrowser {
 };
 
 std::vector<WebHeadBrowser>     web_head_find (WebHeadType type = WebHeadType::Any);
+
+class WebHeadSession {
+public:
+  explicit      WebHeadSession  (const std::string &url, const std::string &appname = "");
+  int           start           (const WebHeadBrowser &browser);
+  bool          running         ();
+  int           kill            (int signal = 1);
+  struct Process;
+  using ProcessP = std::shared_ptr<Process>;
+private:
+  std::string    url_, app_;
+  ProcessP       process_;
+};
+using WebHeadSessionP = std::shared_ptr<WebHeadSession>;
 
 } // WebHead
