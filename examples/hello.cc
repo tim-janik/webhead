a132b15fb8a769685a6943710c74d7c86a53e10b
Tim Janik <timj@gnu.org>
2023-09-19 03:21:09 +0200

    EXAMPLES: hello.cc: show case example Chromium web head
    Signed-off-by: Tim Janik <timj@gnu.org>

--- examples/hello.cc
+++ examples/hello.cc
@@ -8,11 +8,11 @@ using namespace WebHead;
 int
 main (int argc, const char *argv[])
 {
-  const std::vector<WebHeadBrowser>  browsers = web_head_find();
+  const std::vector<WebHeadBrowser>  browsers = web_head_find (WebHeadType::Chromium);
   for (const WebHeadBrowser &b : browsers)
     printf ("Browser: type=%d%s\t%-32s version %-16s\t\t(%s)\n", int (b.type), b.snapdir ? " (snap):" : ":    ", b.executable.c_str(), b.version.c_str(), b.identification.c_str());
   if (browsers.size()) {
-    static WebHeadSession wh (__FILE__, "https://github.com/tim-janik/webhead/blob/trunk/examples/hello.cc");
+    static WebHeadSession wh ("https://github.com/tim-janik/webhead/blob/trunk/examples/hello.cc", __FILE__);
     printf ("%s:%s: starting web head: %s\n", __FILE__, __func__, browsers[0].executable.c_str());
     int err = wh.start (browsers[0]);
     printf ("%s:%s: %s: running=%d: %s\n", __FILE__, __func__, browsers[0].executable.c_str(), wh.running(), strerror (err));
